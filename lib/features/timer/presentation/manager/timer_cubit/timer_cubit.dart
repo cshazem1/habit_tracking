162b8cee03dbc49b753d8d9d269ff7a3cf499ae7
AQ <cshazemelwazery@gmail.com>
2024-09-22 01:20:17 +0300

    done timer screen

--- lib/features/timer/presentation/manager/timer_cubit/timer_cubit.dart
+++ lib/features/timer/presentation/manager/timer_cubit/timer_cubit.dart
@@ -9,61 +9,55 @@ part 'timer_state.dart';
 class TimerCubit extends Cubit<TimerState> {
   TimerCubit() : super(TimerInitial());
   int? maxTime; // Total time in seconds (e.g., 5 minutes)
-  int ?remainingTime ;
-  double? progress ;
+  int? remainingTime;
+  double? progress;
   Timer? timer;
-  bool isPlay=true;
+  bool isPlay = true;
   final player = AudioPlayer();
 
   void startTimer() {
-
-    timer = Timer.periodic(const Duration(seconds: 1), (timer)  {
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       if (remainingTime! > 0) {
-          remainingTime=remainingTime!-1;
-          progress = remainingTime! / maxTime!;
+        remainingTime = remainingTime! - 1;
+        progress = remainingTime! / maxTime!;
 
-          if(remainingTime!>maxTime!/4){
-            player.play(AssetSource('music/videoplayback2.mp3'));
-
-          }
-          else {
-            player.play(AssetSource('music/videoplaybac.mp3'));
-          }
-
-
-          emit(TimerSuccess(text: getFormattedTime()));
+        if (remainingTime! > maxTime! / 4) {
+          player.play(AssetSource('music/videoplayback2.mp3'));
         } else {
-          timer.cancel();
-          isPlay=false;
-          emit(TimerSuccess(text: getFormattedTime()));
+          player.play(AssetSource('music/videoplaybac.mp3'));
         }
-    });
 
+        emit(TimerSuccess(text: getFormattedTime()));
+      } else {
+        timer.cancel();
+        isPlay = false;
+        emit(TimerSuccess(text: getFormattedTime()));
+      }
+    });
   }
 
   void resetTimer() {
     pauseTimer();
     remainingTime = maxTime;
     progress = 1;
-    isPlay=true;
+    isPlay = true;
     startTimer();
     emit(TimerSuccess(text: getFormattedTime()));
   }
 
-void stopAndResetTimer(){
-    if(isPlay){
+  void stopAndResetTimer() {
+    if (isPlay) {
       pauseTimer();
-      isPlay=false;
+      isPlay = false;
       emit(TimerSuccess(text: getFormattedTime()));
-    }
-    else{
+    } else {
       pauseTimer();
       startTimer();
-      isPlay=true;
+      isPlay = true;
       emit(TimerSuccess(text: getFormattedTime()));
-
     }
-}
+  }
+
   void pauseTimer() {
     timer?.cancel();
   }
@@ -73,15 +67,13 @@ void stopAndResetTimer(){
     int seconds = remainingTime! % 60;
     return '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
   }
-void init({required int maxTime,required double progress}){ {
-    this.maxTime = maxTime;
-    remainingTime = maxTime;
-    this.progress = progress;
-    emit(TimerInitial());
+
+  void init({required int maxTime, required double progress}) {
+    {
+      this.maxTime = maxTime;
+      remainingTime = maxTime;
+      this.progress = progress;
+      emit(TimerInitial());
+    }
   }
-
-
-}
-
-
 }

f036bead4361a6ac7bea89bece865e2fd7a1c609
AQ <cshazemelwazery@gmail.com>
2024-09-29 01:44:10 +0300

    added nadeen $ nada $ hazem

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:habit_tracking/features/login%20screen/presentation/login_screen.dart';
-import 'package:habit_tracking/features/onboarding/presentation/onboardin_view.dart';
-import 'package:habit_tracking/features/register/presentation/register_view.dart';
-import 'package:habit_tracking/features/starting%20page/presentation/starting_view.dart';
-import 'features/timer/presentation/timer_views.dart';
+
+import 'core/routes/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (BuildContext context) {
+      return MyApp();
+    },
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,14 +21,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      child: GetMaterialApp(
-        useInheritedMediaQuery: true,
-        locale: DevicePreview.locale(context),
-        builder: DevicePreview.appBuilder,
-        theme: ThemeData.light(),
-        darkTheme: ThemeData.dark(),
-        home:  OnboardinView(),
-    ););}}
+        designSize: const Size(375, 812),
+        child: GetMaterialApp(
+          onGenerateRoute: AppRouter.onGenerateRoute,
+          useInheritedMediaQuery: true,
+          locale: DevicePreview.locale(context),
+          builder: DevicePreview.appBuilder,
+          theme: ThemeData.light(),
+          darkTheme: ThemeData.dark(),
+        ));
+  }
+}
